27bc214bfbd139925386adad0b6d05cc48535fa7
sskotezhov <sskotezhov@edu.hse.ru>
2024-02-21 15:44:39 +0300

    format git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admi/Desktop/test
-# Build directory: /home/admi/Desktop/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
